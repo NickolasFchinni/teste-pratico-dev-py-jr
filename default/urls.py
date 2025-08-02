b91f3bd817c01b898fbf1c08d6454d04c2790506
Nickolas <nulleyson@gmail.com>
2025-08-02 18:05:15 -0300

    feat: Criando página para detalhes da tarefa, esboço

--- default/urls.py
+++ default/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ListaDeTarefas
+from .views import ListaDeTarefas, DetalhesDaTarefa
 
 urlpatterns = [
   path('', ListaDeTarefas.as_view(), name='tarefas'),
+  path('tarefa/<int:pk>/', DetalhesDaTarefa.as_view(), name='tarefas'),
 ]
--- default/views.py
+++ default/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Tarefa
 
 class ListaDeTarefas(ListView):
     model = Tarefa
     context_object_name = 'tarefas'
 
+class DetalhesDaTarefa(DetailView):
+    model = Tarefa
+    context_object_name = 'tarefa'
+    template_name = 'default/tarefa.html'
