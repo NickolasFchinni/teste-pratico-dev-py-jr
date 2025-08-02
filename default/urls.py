b50c642f84ce66d876edb36a60ecc48ada6436d3
Nickolas <nulleyson@gmail.com>
2025-08-02 20:30:45 -0300

    feat: Implementando Login e Logout

--- default/urls.py
+++ default/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
-from .views import ListaDeTarefas, DetalhesDaTarefa, CriarTarefa, AtualizarTarefa, DeletarTarefa
+from .views import ListaDeTarefas, DetalhesDaTarefa, CriarTarefa, AtualizarTarefa, DeletarTarefa, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+  path('login/', CustomLoginView.as_view(), name='login'),
+  path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+
+
   path('', ListaDeTarefas.as_view(), name='tarefas'),
   path('tarefa/<int:pk>/', DetalhesDaTarefa.as_view(), name='tarefa'),
   path('editar-tarefa/<int:pk>/', AtualizarTarefa.as_view(), name='editar-tarefa'),
   path('deletar-tarefa/<int:pk>/', DeletarTarefa.as_view(), name='deletar-tarefa'),
-  path('criar-tarefa/', CriarTarefa.as_view(), name='criar-tarefa'),
+  path('criar-tarefa/', CriarTarefa.as_view(), name='criar-tarefa')
 ]
--- default/views.py
+++ default/views.py
@@ -3,7 +3,26 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+# Imports for Reordering Feature
+from django.views import View
+from django.shortcuts import redirect
+from django.db import transaction
+
 from .models import Tarefa
+
+class CustomLoginView(LoginView):
+    template_name = 'default/login.html'
+    fields = "__all__"
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tarefas')
 
 class ListaDeTarefas(ListView):
     model = Tarefa
@@ -18,7 +37,6 @@ class CriarTarefa(CreateView):
     model = Tarefa
     fields = '__all__'
     success_url = reverse_lazy('tarefas')
-
 class AtualizarTarefa(UpdateView):
     model = Tarefa
     fields = '__all__'
